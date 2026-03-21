f6368624ab527f92de57a381bb731cd3d3eba9fe
vince <paulag10987@gmail.com>
2026-03-21 20:04:10 +0800

    chore(py): add sample data

--- week13_todo_api/app.py
+++ week13_todo_api/app.py
@@ -4,7 +4,14 @@ app = Flask(__name__)
 @app.route('/', methods=['GET']) # type: ignore
 
 def home():
-    pass
+    response_data = {
+        "status": "success",
+        "message": "TODO App is UP",
+        "version": "1.0"
+    }
+    
+    return jsonify(response_data)
 
 if __name__ == '__main__':
-    pass
+    print("Starting TODO Backend Server")
+    app.run(debug=True, port=5000)
