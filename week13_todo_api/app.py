924f82bcec6609af5941aa90337a5cce50c773fe
vinny <paulag10987@gmail.com>
2026-03-22 09:50:28 +0800

    chore(py): add temporary database

--- week13_todo_api/app.py
+++ week13_todo_api/app.py
@@ -1,5 +1,10 @@
 from flask import Flask, jsonify
 
+todos = [
+    {"id": 1, "task": "Learn Flask", "completed": True},
+    {"id": 2, "task": "Build GET and POST endpoints", "completed":False}
+]
+
 app = Flask(__name__)
 @app.route('/', methods=['GET']) # type: ignore
 
@@ -9,7 +14,7 @@ def home():
         "message": "TODO App is UP",
         "version": "1.0"
     }
-    
+
     return jsonify(response_data)
 
 if __name__ == '__main__':
