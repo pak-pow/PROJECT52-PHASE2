b0d5c24abb74301c5ca24eb88019bd2b6c888bb8
vince <paulag10987@gmail.com>
2026-03-24 10:31:59 +0800

    chore: add type ignore to remove yellow underlines

--- week13_todo_api/app.py
+++ week13_todo_api/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS 
+from flask_cors import CORS #type: ignore
 
 todos = [
     {"id": 1, "task": "Learn Flask", "completed": True},
