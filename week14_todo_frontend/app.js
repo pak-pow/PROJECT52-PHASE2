c0dc711a70bc89a5b2c47eedda05359022e263df
vince <paulag10987@gmail.com>
2026-03-27 17:12:23 +0800

    feat: implement loadTodos function to fetch and display todo items from API

--- week14_todo_frontend/app.js
+++ week14_todo_frontend/app.js
@@ -2,5 +2,23 @@ const API_URL = 'http://127.0.0.1:5000/todos';
 const todoList = document.getElementById('todoList');
 
 asynch function loadTodos(){
-    
+
+    try {
+
+        // getting a get response from the local server
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        
+        // clearing the list on the screen so we don't get duplicates
+        todoList.innerHTML = '';
+
+        data.todos.forEach(todo => {
+            const li = document.createElement('li');
+        });
+
+
+    }
+
 }
+
+loadTodos();
