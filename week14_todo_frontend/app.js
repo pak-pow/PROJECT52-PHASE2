4f02bdc55a92a289bf8f3169f367b9d136370129
vince <paulag10987@gmail.com>
2026-03-28 11:13:06 +0800

    refactor: reorganize code structure and improve readability in app.js

--- week14_todo_frontend/app.js
+++ week14_todo_frontend/app.js
@@ -1,10 +1,14 @@
+// URL Api
 const API_URL = 'http://127.0.0.1:5000/todos';
+
+// grabbing the html elements so js can control
 const todoList = document.getElementById('todoList');
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById('addBtn');   
 
+// GET / Read tasks
 async function loadTodos(){
-
     try {
-
         // getting a get response from the local server
         const response = await fetch(API_URL);
         const data = await response.json();
@@ -21,7 +25,10 @@ async function loadTodos(){
         console.error("Error connecting to the backend: ", error);
         todoList.innerHTML = "<li> Could not connect to the server. Is Python running?</li>";
     }
+}
 
+// POST / create tasks
+async function addTodo() {
 }
 
 loadTodos();
