5e52bad27dfbb2df11da18eac86070bc92c3f940
vince <paulag10987@gmail.com>
2026-04-05 13:32:51 +0800

    feat: add authentication endpoints
    - Added routes to register and authenticate users in the backend of the week15 Project

--- week15_auth_system/app.py
+++ week15_auth_system/app.py
@@ -6,3 +6,17 @@ import sqlite3                              #type: ignore
 app = Flask(__name__)
 CORS(app)
 
+app.config['SECRET_KEY'] = 'super-secret-project52-key'
+DB_NAME = 'users.db'
+
+def init_db():
+    pass
+
+@app.route('/register', methods=['POST'])
+def register():
+    pass
+
+if __name__ == '__main__':
+    init_db()
+    print("Auth Server Running!")
+    app.run(debug=True, port=5000)
