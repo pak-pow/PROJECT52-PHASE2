d66b7290f9f92339b0ab13cd8b0c7903a1f74424
vince <paulag10987@gmail.com>
2026-04-21 10:47:13 +0800

    feat: add home / base route

--- week17_sqlite_manager/app.py
+++ week17_sqlite_manager/app.py
@@ -1,3 +1,10 @@
 from flask import Flask, render_template, request, jsonify # type: ignore
 from db_manager import DatabaseManager
 
+app = Flask(__name__)
+db = DatabaseManager("project52.db")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
