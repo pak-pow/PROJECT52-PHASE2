af9a884792b15ce17bb0123309e4fdf0be8eb8da
vince <paulag10987@gmail.com>
2026-05-04 19:40:36 +0800

    feat: add functions

--- week19_markdown_notes/backend/app.py
+++ week19_markdown_notes/backend/app.py
@@ -3,8 +3,14 @@ app = Flask(__name__)
 
 @app.after_request
 def add_cors(response):
-    pass
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
 @app.route('/api/notes', methods=['GET'])
 def get_notes():
-    pass
+    return jsonify({
+        "message": "Hello from file system!"
+    })
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
