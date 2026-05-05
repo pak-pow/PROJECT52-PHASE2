60d141df48b99bf79ebb8261c944f3926fcd2a1b
vince <paulag10987@gmail.com>
2026-05-05 20:57:59 +0800

    feat: refactor other endpoints and add new import and added new file directory

--- week19_markdown_notes/backend/app.py
+++ week19_markdown_notes/backend/app.py
@@ -1,5 +1,11 @@
-from flask import Flask, jsonify # type: ignore
+from flask import Flask, jsonify, request # type: ignore
+import os
 app = Flask(__name__)
+
+DATA_DIR = "data"
+
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
 
 @app.after_request
 def add_cors(response):
@@ -7,10 +13,20 @@ def add_cors(response):
     return response
 
 @app.route('/api/notes', methods=['GET'])
-def get_notes():
-    return jsonify({
-        "message": "Hello from file system!"
-    })
+def list_notes():
+    try:
+        files = [f for f in os.listdir(DATA_DIR) if f.endswith('.md')]
+        return jsonify({"notes": files})
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/api/notes/<filename>', methods=['GET'])
+def read_notes(filename):
+    filepath = os.path.join(DATA_DIR, filename)
+    
+    if not os.path.exists(filepath):
+        return jsonify({"error": "File not Found"}), 404
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
