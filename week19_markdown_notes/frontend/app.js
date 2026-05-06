2e2b75aa20d286992ba18fbbfdac23bb984e8030
vince <paulag10987@gmail.com>
2026-05-06 20:04:06 +0800

    feat: added function with the ability to write in the site itself

--- week19_markdown_notes/frontend/app.js
+++ week19_markdown_notes/frontend/app.js
@@ -1,17 +1,43 @@
 const noteList = document.getElementById('note-list');
+const editorContainer = document.getElementById('editor-container');
+const welcomeText = document.getElementById('welcome-text');
+const titleInput = document.getElementById('note-title-input');
+const markdownEditor = document.getElementById('markdown-editor');
+
 async function loadNoteList() {
     try {
         const response = await fetch('http://127.0.0.1:5000/api/notes');
         const data = await response.json();
+        
         noteList.innerHTML = '';
+        
         data.notes.forEach(filename => {
             const li = document.createElement('li');
             li.textContent = filename;
             li.classList.add('note-item'); 
+            
+            li.addEventListener('click', () => loadNoteContent(filename));
+            
             noteList.appendChild(li);
         });
     } catch (error) {
         console.error("Failed to connect to backend:", error);
+    }
+}
+
+async function loadNoteContent(filename) {
+    try {
+        const response = await fetch(`http://127.0.0.1:5000/api/notes/${filename}`);
+        const data = await response.json();
+        
+        welcomeText.style.display = 'none';
+        editorContainer.style.display = 'flex';
+    
+        titleInput.value = data.filename;
+        markdownEditor.value = data.content;
+        
+    } catch (error) {
+        console.error("Failed to load note:", error);
     }
 }
 
