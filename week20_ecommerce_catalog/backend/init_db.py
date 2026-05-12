a802f25e9bce7271ccdf1497b9319fa4d5ac064f
vince <paulag10987@gmail.com>
2026-05-12 20:10:57 +0800

    feat: add script

--- week20_ecommerce_catalog/backend/init_db.py
+++ week20_ecommerce_catalog/backend/init_db.py
@@ -1,4 +1,16 @@
 import sqlite3
 
 def initialize_db():
-    pass
+    
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS products (
+            id          INTEGER PRIMARY KEY AUTOINCREMENT,
+            name        TEXT NOT NULL,
+            category    TEXT NOT NULL,
+            price       REAL NOT NULL,
+            image       TEXT NOT NULL
+        )
+    ''')
