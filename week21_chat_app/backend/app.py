cfe87f57cfe5260beaa474f18304f9faec264998
vince <paulag10987@gmail.com>
2026-05-19 19:05:54 +0800

    feat: added new socket for typing

--- week21_chat_app/backend/app.py
+++ week21_chat_app/backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask #type:ignore
-from flask_socketio import SocketIO, send #type: ignore
+from flask_socketio import SocketIO, emit #type: ignore
 from flask_cors import CORS #type: ignore
 
 app = Flask(__name__)
@@ -11,6 +11,10 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 def handle_chat_message(data):
     print(f"Server Received: {data}")
     socketio.emit('chat_message', data)
+    
+@socketio.on('typing')
+def handle_typing(data):
+    emit('typing', data, broadcast=True, include_self=False)
 
 if __name__ == '__main__':
     socketio.run(app, port=5000, debug=True)
