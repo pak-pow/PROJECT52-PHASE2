f6fe9a2df07b1b1ec69da1f5e5391ed112ccc56e
vinny <paulag10987@gmail.com>
2026-05-20 16:50:20 +0800

    feat: add new endpoint for the server had joined

--- week21_chat_app/backend/app.py
+++ week21_chat_app/backend/app.py
@@ -1,5 +1,5 @@
-from flask import Flask #type:ignore
-from flask_socketio import SocketIO, send
+from flask import Flask, request
+from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,10 +7,26 @@ app.config['SECRET_KEY'] = 'p52_chat_secret'
 
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+active_users = {}
+
+@socketio.on('user_join')
+def handle_user_join(username):
+    active_users[request.sid] = username
+    emit('system_message', f"{username} joined the chat", broadcast=True)
+
 @socketio.on('chat_message')
 def handle_chat_message(data):
-    print(f"Server Received: {data}")
     socketio.emit('chat_message', data)
+
+@socketio.on('typing')
+def handle_typing(data):
+    emit('typing', data, broadcast=True, include_self=False)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    if request.sid in active_users:
+        username = active_users.pop(request.sid)
+        emit('system_message', f"{username} left the chat", broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, port=5000, debug=True)
