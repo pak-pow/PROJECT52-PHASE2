ccde33169ccff1aa427c02159c9ee0a941c907e5
vince <paulag10987@gmail.com>
2026-05-23 11:27:04 +0800

    feat: add simple endpoint for health status

--- week22_cms_blog/backend/app/__init__.py
+++ week22_cms_blog/backend/app/__init__.py
@@ -2,4 +2,16 @@ from flask import Flask #type:ignore
 from .extensions.db import close_db, init_db
 
 def create_app():
-    pass
+    
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+    
+    init_db(app)
+    
+    app.teardown_appcontext(close_db)
+    
+    @app.route('/api/health')
+    def health():
+        return {"status": "healthy", "architecture": "Deeply Nested File Structure"}
+    
+    return app
