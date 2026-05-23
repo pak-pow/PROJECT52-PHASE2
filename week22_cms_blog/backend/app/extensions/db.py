e0b308f6b68b72a55437637c003f3f03292dc5d8
vince <paulag10987@gmail.com>
2026-05-23 11:22:47 +0800

    feat: add handling of db functionality to db.py

--- week22_cms_blog/backend/app/extensions/db.py
+++ week22_cms_blog/backend/app/extensions/db.py
@@ -3,10 +3,34 @@ import os
 from flask import g, current_app #type: ignore
 
 def get_db():
-    pass
+    
+    if 'db' not in g:
+        
+        # getting the database file directory from the config file
+        g.db = sqlite3.connect(current_app.config['DATABASE'])
+        g.db.row_factory = sqlite3.Row
+        
+    return g.db
 
 def close_db():
-    pass
+    
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
 
-def init_db():
-    pass
+def init_db(app):
+    db_path = app.config['DATABASE']
+    
+    if not os.path.exists(db_path):
+        print ("Initializing the Database...") 
+        
+        with app.app_context():
+            db = get_db()
+            
+            schema_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'schema.sql')
+            with open(schema_path, 'r') as f:
+                db.executescript(f.read())
+                
+            db.commit()
+            
+        print("Database built successfully")
