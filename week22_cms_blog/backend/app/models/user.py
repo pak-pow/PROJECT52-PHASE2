c566afd46c7af6548037341ec37e9a49e244faa5
vince <paulag10987@gmail.com>
2026-05-24 11:03:57 +0800

    feat: add sql script execution

--- week22_cms_blog/backend/app/models/user.py
+++ week22_cms_blog/backend/app/models/user.py
@@ -2,9 +2,11 @@ from app.extensions.db import get_db
 
 class User:
     @staticmethod
-    def get_by_username():
-        pass
+    def get_by_username(username):
+        db = get_db()
+        return db.execute('SELECT * FROM users WHERE username = ?', (username,)).fetchone()
     
     @staticmethod
-    def get_by_id():
-        pass
+    def get_by_id(user_id):
+        db = get_db()
+        return db.execute('SELECT * FROM users WHERE id = ?', (user_id,)).fetchone() 
