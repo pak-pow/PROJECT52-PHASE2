6e89e83fb3205a5c2471cadbb525cc8c61280b08
vince <paulag10987@gmail.com>
2026-05-24 11:09:02 +0800

    feat: add getposts functionality

--- week22_cms_blog/backend/app/api/posts.py
+++ week22_cms_blog/backend/app/api/posts.py
@@ -5,4 +5,17 @@ posts_bp = Blueprint('posts', __name__)
 
 @posts_bp.route('/', methods=['GET'])
 def get_posts():
-    pass
+    
+    raw_posts = Post.get_all()
+    
+    posts_list = []
+    for post in raw_posts:
+        posts_list.append({
+            "id": post['id'],
+            "title": post['title'],
+            "content": post['content'],
+            "author_id": post['author_id'],
+            "created_at": post['created_at']
+        })
+        
+    return jsonify(posts_list), 200
