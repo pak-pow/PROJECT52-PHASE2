7485736d0ca85c9a86a343f7b616dc11d706a819
vince <paulag10987@gmail.com>
2026-05-30 14:13:49 +0800

    feat: add password hasing on backend

--- week22_cms_blog/backend/app/models/user.py
+++ week22_cms_blog/backend/app/models/user.py
@@ -1,4 +1,5 @@
 from app.extensions.db import get_db
+from werkzeug.security import check_password_hash  # type: ignore
 
 class User:
     @staticmethod
@@ -10,3 +11,10 @@ class User:
     def get_by_id(user_id):
         db = get_db()
         return db.execute('SELECT * FROM users WHERE id = ?', (user_id,)).fetchone() 
+    
+    @staticmethod
+    def verify_password(username, raw_password):
+        user = User.get_by_username(username)
+        if user and check_password_hash(user['password'], raw_password):
+            return user
+        return None
