1d794efb6212aa7ce08e880eba223bd558404678
vince <paulag10987@gmail.com>
2026-05-30 14:23:04 +0800

    chore: correct column

--- week22_cms_blog/backend/app/models/user.py
+++ week22_cms_blog/backend/app/models/user.py
@@ -1,20 +1,20 @@
 from app.extensions.db import get_db
-from werkzeug.security import check_password_hash  # type: ignore
+from werkzeug.security import check_password_hash #type: ignore
 
 class User:
     @staticmethod
     def get_by_username(username):
         db = get_db()
         return db.execute('SELECT * FROM users WHERE username = ?', (username,)).fetchone()
-    
+
     @staticmethod
     def get_by_id(user_id):
         db = get_db()
-        return db.execute('SELECT * FROM users WHERE id = ?', (user_id,)).fetchone() 
-    
+        return db.execute('SELECT * FROM users WHERE id = ?', (user_id,)).fetchone()
+
     @staticmethod
     def verify_password(username, raw_password):
         user = User.get_by_username(username)
-        if user and check_password_hash(user['password'], raw_password):
+        if user and check_password_hash(user['password_hash'], raw_password):
             return user
         return None
