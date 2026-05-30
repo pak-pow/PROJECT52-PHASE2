d6941cebc7757740be05b8177b1f5b211fb7dfa2
vince <paulag10987@gmail.com>
2026-05-30 15:24:47 +0800

    chore: added a order by asc

--- week22_cms_blog/backend/app/models/post.py
+++ week22_cms_blog/backend/app/models/post.py
@@ -5,8 +5,8 @@ class Post:
     def get_all(status_filter=None):
         db = get_db()
         if status_filter:
-            return db.execute('SELECT * FROM posts WHERE status = ? ORDER BY created_at DESC', (status_filter,)).fetchall()
-        return db.execute('SELECT * FROM posts ORDER BY created_at DESC').fetchall()
+            return db.execute('SELECT * FROM posts WHERE status = ? ORDER BY id ASC', (status_filter,)).fetchall()        
+        return db.execute('SELECT * FROM posts ORDER BY id ASC').fetchall()
 
     @staticmethod
     def get_by_id(post_id):
