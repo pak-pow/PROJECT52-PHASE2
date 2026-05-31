4c2af6d4ccfe5d2e1dffa9fa6af25e50b42186b6
vince <paulag10987@gmail.com>
2026-05-31 15:51:44 +0800

    feat: add functionality on the helper functions on db.py

--- week23_expense_tracker/backend/app/utils/db.py
+++ week23_expense_tracker/backend/app/utils/db.py
@@ -2,7 +2,15 @@ import sqlite3
 from flask import g, current_app #type:ignore
 
 def get_db():
-    pass
+    if 'db' not in g:
+        g.db = sqlite3.connect(current_app.config['DATABASE'])
+        
+        # this returns a row as a dictionary not a tuple
+        g.db.row_factory = sqlite3.Row 
+    
+    return g.db
 
 def close_db():
-    pass
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
