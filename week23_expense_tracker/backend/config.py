17dabaf51e42864a0904043a87336b1498d87724
vince <paulag10987@gmail.com>
2026-05-31 15:45:47 +0800

    feat: add secrets variables

--- week23_expense_tracker/backend/config.py
+++ week23_expense_tracker/backend/config.py
@@ -5,4 +5,6 @@ load_dotenv()
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config: 
-    pass
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key-week23'
+    JWS_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'super-secret-jwt-key-week23'
+    DATABASE = os.path.join(BASE_DIR, 'data', 'database.db')
