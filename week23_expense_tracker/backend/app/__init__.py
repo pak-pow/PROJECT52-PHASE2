751a83834c39824c99f4bed3a960e1ae3ce9af1b
vince <paulag10987@gmail.com>
2026-06-01 15:25:45 +0800

    feat: add jwt to init

--- week23_expense_tracker/backend/app/__init__.py
+++ week23_expense_tracker/backend/app/__init__.py
@@ -3,12 +3,15 @@ from flask_cors import CORS #type: ignore
 from config import Config #type: ignore
 from .utils.db import close_db
 from .routes.expenses import expenses_bp 
+from flask_jwt_extended import JWTManager #type: ignore
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
     
     app.config.from_object(Config)
+    jwt = JWTManager(app)
+    
     app.teardown_appcontext(close_db)
     app.register_blueprint(expenses_bp, url_prefix='/api/expenses')
     
