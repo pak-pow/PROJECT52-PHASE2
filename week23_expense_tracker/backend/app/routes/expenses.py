cf4bf75071d0d2caf038518812e7888003ca1fbe
vince <paulag10987@gmail.com>
2026-06-01 15:46:36 +0800

    feat: add jwt required to the expenses tracker route

--- week23_expense_tracker/backend/app/routes/expenses.py
+++ week23_expense_tracker/backend/app/routes/expenses.py
@@ -1,40 +1,45 @@
 from flask import Blueprint, jsonify, request #type: ignore
 from app.models.expense import Expense
+from flask_jwt_extended import jwt_required, get_jwt_identity #type: ignore
 
 expenses_bp = Blueprint('expenses', __name__)
 
-CURRENT_USER_ID = 1 
-
 @expenses_bp.route('/', methods=['GET'])
+@jwt_required() 
 def get_expenses():
-    """Returns all raw expenses for the current user."""
-    raw_expenses = Expense.get_all_by_user(CURRENT_USER_ID)
-
-    expenses_list = [dict(row) for row in raw_expenses]
-    return jsonify(expenses_list), 200
+    current_user_id = get_jwt_identity() 
+    raw_expenses = Expense.get_all_by_user(current_user_id)
+    return jsonify([dict(row) for row in raw_expenses]), 200
 
 @expenses_bp.route('/summary', methods=['GET'])
+@jwt_required() 
 def get_expense_summary():
-    """Returns the aggregated total amount per category for Chart.js."""
-    summary_data = Expense.get_aggregated_by_category(CURRENT_USER_ID)
-    
-    summary_list = [dict(row) for row in summary_data]
-    return jsonify(summary_list), 200
+    current_user_id = get_jwt_identity()
+    summary_data = Expense.get_aggregated_by_category(current_user_id)
+    return jsonify([dict(row) for row in summary_data]), 200
 
 @expenses_bp.route('/', methods=['POST'])
+@jwt_required() 
 def add_expense():
-    """Logs a new expense into the database."""
     data = request.get_json()
+    current_user_id = get_jwt_identity()
     
     if not data or not data.get('amount') or not data.get('category') or not data.get('date'):
-        return jsonify({"error": "Missing required fields: amount, category, date"}), 400
+        return jsonify({"error": "Missing required fields"}), 400
         
     new_id = Expense.create(
-        user_id=CURRENT_USER_ID,
+        user_id=current_user_id,
         amount=data['amount'],
         category=data['category'],
         description=data.get('description', ''),
         date=data['date']
     )
-    
     return jsonify({"message": "Expense logged successfully!", "id": new_id}), 201
+
+@expenses_bp.route('/<int:expense_id>', methods=['DELETE'])
+@jwt_required()
+def delete_expense(expense_id):
+    current_user_id = get_jwt_identity()
+    Expense.delete(expense_id, current_user_id)
+    
+    return jsonify({"message": f"Expense {expense_id} deleted successfully!"}), 200
