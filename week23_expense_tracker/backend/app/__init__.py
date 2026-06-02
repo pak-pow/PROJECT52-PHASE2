3d96b105eebe859fd60d363f6e949f66213524ee
vince <paulag10987@gmail.com>
2026-06-02 12:06:20 +0800

    feat: add auth route

--- week23_expense_tracker/backend/app/__init__.py
+++ week23_expense_tracker/backend/app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask #type: ignore
 from flask_cors import CORS #type: ignore
 from config import Config #type: ignore
 from .utils.db import close_db
-from .routes.expenses import expenses_bp 
+from .routes.expenses import expenses_bp
+from .routes.auth import auth_bp 
 from flask_jwt_extended import JWTManager #type: ignore
 
 def create_app():
@@ -14,6 +15,7 @@ def create_app():
     
     app.teardown_appcontext(close_db)
     app.register_blueprint(expenses_bp, url_prefix='/api/expenses')
+    app.register_blueprint(auth_bp, url_prefix='api/auth')
     
     @app.route('/api/health')
     def health_check():
