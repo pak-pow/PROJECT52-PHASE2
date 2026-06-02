2c54b42fd9bfdc8cda7494bd7d8d9b675cf9fe61
vince <paulag10987@gmail.com>
2026-06-02 20:03:06 +0800

    feat:: add response checker

--- week23_expense_tracker/frontend/src/api/client.js
+++ week23_expense_tracker/frontend/src/api/client.js
@@ -17,6 +17,20 @@ export const apiClient = async (endpoint, options = {}) => {
 
     try {
         const response = await fetch(`${BASE_URL}${endpoint}`, config);
+
+        if (response.status == 401) {
+            console.error("Session expired or unauthorized. Logging out.");
+            localStorage.removeItem('jwt_token');
+            window.location.href = '/login.html';
+            return null;
+        }
+        
+        const data = await response.json()
+        if (!response.ok) {
+            throw new Error(data.error || 'API Request Failed')
+        }
+        return data;
+
     } catch (error) {
         console.error(`[API Error] ${endpoint}:`, error.message);
         throw error;
