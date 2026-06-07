54a9787fa03c319f89de32f98a0024bfea98043f
vince <paulag10987@gmail.com>
2026-06-07 10:39:54 +0800

    feat: add test case for decoding

--- week24_url_shortener/backend/tests/test_base62.py
+++ week24_url_shortener/backend/tests/test_base62.py
@@ -14,3 +14,18 @@ def test_encode_zero_or_negative():
         
     with pytest.raises(ValueError):
         encode(-5)
+        
+def test_encode_decode_symmetry():
+    """The golden rule: is that decoding an encoded string must return the original number"""
+    test_number = [1,42,100, 9999,123456789]
+    
+    for num in test_number:
+        encoded_str = encode(num)
+        decoded_num = decode(encoded_str)
+        
+        assert decoded_num == num, f"Failed on {num}. Got {decoded_num} instead"
+        
+def test_decode_invalid_string():
+    """Decoding a string with invalid characters (like symbols) should fail"""
+    with pytest.raises(ValueError):
+        decode("2B@i!")
