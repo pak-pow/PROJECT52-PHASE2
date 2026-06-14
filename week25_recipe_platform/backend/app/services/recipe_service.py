6d6badff5c823d6582b8dfff76253735cfa42204
vince <paulag10987@gmail.com>
2026-06-14 13:38:26 +0800

    feat: cleaned the data before returning and also add a new method allowed_files

--- week25_recipe_platform/backend/app/services/recipe_service.py
+++ week25_recipe_platform/backend/app/services/recipe_service.py
@@ -15,3 +15,14 @@ def validate_recipe_data(data: dict) -> dict: # type: ignore
     for field in required_fields:
         if field not in data or not str(data[field]).strip():
             raise ValidationError(f"{field} is required and cannot be empty")
+        
+    return {
+        'title': data['title'].strip(),
+        'description': data['description'].strip(),
+        'ingredients': data['ingredients'].strip(),
+        'instruction': data['instruction'].strip()
+    }
+    
+def allowed_file(filename: str, allowed_extensions: str) -> bool: #type:ignore
+    pass
+    
