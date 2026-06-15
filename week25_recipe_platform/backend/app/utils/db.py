687ef8a7b95e835a41a784229aa0391d86c86c6e
vince <paulag10987@gmail.com>
2026-06-15 15:54:59 +0800

    feat: add 2 methods for initializing the db

--- week25_recipe_platform/backend/app/utils/db.py
+++ week25_recipe_platform/backend/app/utils/db.py
@@ -3,3 +3,20 @@ import os
 from flask import g #type: ignore
 
 BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+DB_PATH = os.path.join(BASE_DIR, 'data', 'database.db')
+SCHEMA_PATH = os.path.join(BASE_DIR, 'data', 'schema.sql') 
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(DB_PATH)
+        g.db.row_factory = sqlite3.Row
+        
+    return g.db
+
+def init_db():
+    db = get_db()
+    
+    with open(SCHEMA_PATH, 'r') as f:
+        db.executescript(f.read())
+        
+    db.commit
