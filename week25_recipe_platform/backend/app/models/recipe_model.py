6f3f11054e18e29fb9e2a28e99aa99133c705bc3
vince <paulag10987@gmail.com>
2026-06-15 15:56:48 +0800

    feat: add recipe model

---
+++ week25_recipe_platform/backend/app/models/recipe_model.py
@@ -0,0 +1,20 @@
+from app.utils import db
+
+def insert_recipe(title: str, description: str, ingredients: str, instructions: str, image_filename: str = None) -> dict: # type: ignore
+    conn = db.get_db()
+    cursor = conn.execute(
+        """
+        INSERT INTO recipes (title, description, ingredients, instructions, image_filename)
+        VALUES (?, ?, ?, ?, ?)
+        """,
+        (title, description, ingredients, instructions, image_filename)
+    )
+    conn.commit()
+    
+    new_id = cursor.lastrowid
+    return get_recipe_by_id(new_id) #type: ignore
+
+def get_recipe_by_id(recipe_id: int) -> dict:
+    conn = db.get_db()
+    cursor = conn.execute("SELECT * FROM recipes WHERE id = ?", (recipe_id,))
+    return dict(cursor.fetchone())
