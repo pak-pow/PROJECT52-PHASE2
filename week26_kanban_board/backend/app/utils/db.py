909792c4032c33955e402e5bfc2496a2d439d48d
vince <paulag10987@gmail.com>
2026-06-20 11:47:22 +0800

    feat: add getDB method by checking directory using os.path.dirname

--- week26_kanban_board/backend/app/utils/db.py
+++ week26_kanban_board/backend/app/utils/db.py
@@ -3,7 +3,18 @@ import os
 from flask import g, current_app #type: ignore
 
 def get_db():
-    pass
+    if 'db' not in g:
+        base_dir = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+        db_path = os.path.join(base_dir, 'data', 'kanban.db')
+        
+        g.db = sqlite3.connect(
+            db_path,
+            detect_types = sqlite3.PARSE_DECLTYPES
+        ) 
+        g.db.row_factory = sqlite3.Row
+        g.db.execute('PRAGMA foreign_keys = ON;')
+        
+    return g.db
 
 def close_db():
     pass
