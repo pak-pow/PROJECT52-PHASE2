3b7427a2be56be586c4980b0ea7dc552f7e80644
vince <paulag10987@gmail.com>
2026-06-20 12:24:19 +0800

    feat(init): register blueprint to the init

--- week26_kanban_board/backend/app/__init__.py
+++ week26_kanban_board/backend/app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask #type: ignore
 from flask_cors import CORS #type: ignore
 from app.config import settings
+
 from app.utils.db import init_db, close_db
+from app.routes.board_routes import board_bp
+from app.routes.column_routes import column_bp
+from app.routes.card_routes import card_bp        
 
 def create_app():
     
@@ -10,6 +14,10 @@ def create_app():
     
     with app.app_context():
         init_db()
+
+        app.register_blueprint(board_bp)
+        app.register_blueprint(column_bp)
+        app.register_blueprint(card_bp)
         
     app.teardown_appcontext(close_db)
     return app
