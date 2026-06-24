a1fe8efe7b60edd14c7ba4661a3f542dc8d9d58b
vince <paulag10987@gmail.com>
2026-06-24 12:09:12 +0800

    feat(schema): add users and session on schema file

--- week26_kanban_board/backend/data/schema.sql
+++ week26_kanban_board/backend/data/schema.sql
@@ -1,3 +1,17 @@
+CREATE TABLE IF NOT EXISTS users (
+    id              INTEGER PRIMARY KEY AUTOINCREMENT,
+    username        TEXT UNIQUE NOT NULL,
+    password_hash   TEXT NOT NULL, 
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE IF NOT EXISTS sessions (
+    token           TEXT PRIMARY KEY,
+    user_id         INTEGER NOT NULL,
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
 CREATE TABLE IF NOT EXISTS boards (
     id              INTEGER PRIMARY KEY AUTOINCREMENT,
     title           TEXT NOT NULL,
