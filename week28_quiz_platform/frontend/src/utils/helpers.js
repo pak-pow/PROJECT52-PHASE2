de9748828b6ebac96d7d904cc4f56f438db32c99
vince <paulag10987@gmail.com>
2026-07-08 13:05:53 +0800

    chore: add helper

--- week28_quiz_platform/frontend/src/utils/helpers.js
+++ week28_quiz_platform/frontend/src/utils/helpers.js
@@ -30,3 +30,13 @@ export function getRankClass(rank) {
     if (rank === 3) return "rank-bronze";
     return "";
 }
+
+export function escapeHtml(str) {
+    if (typeof str !== "string") return str;
+    return str
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
